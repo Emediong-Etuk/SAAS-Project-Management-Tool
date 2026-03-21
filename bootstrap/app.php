f0b5131ea7816cda18f9ef13e6d58db55b39eba6
Emediong-Etuk <emedionggregory12@gmail.com>
2026-03-21 21:54:08 +0100

    return all error messages on 500 status code

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,9 +11,9 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -22,7 +22,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->append([
             ForceJsonResponse::class,
         ]);
-
     })
     ->withExceptions(function (Exceptions $exceptions): void {
 
@@ -44,6 +43,9 @@ return Application::configure(basePath: dirname(__DIR__))
                     'message' => 'Server Error',
                     // 'error' => 'An error occured. Please try again later',
                     'error' => $e->getMessage(),
+                    'file' => $e->getFile(),
+                    'line' => $e->getLine(),
+                    'code' => $e->getCode(),
                 ], Response::HTTP_INTERNAL_SERVER_ERROR);
             }
         });
