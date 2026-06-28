248822b4d47c27b0bbe3bcc30cbf79ee926b03ab
Emediong-Etuk <emedionggregory12@gmail.com>
2026-06-28 16:59:51 +0100

    updated cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use App\Http\Middleware\ForceJsonResponse;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\HandleCors;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Sentry\Laravel\Integration;
@@ -28,7 +29,9 @@ return Application::configure(basePath: dirname(__DIR__))
             ForceJsonResponse::class,
         ]);
     })
-
+    ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->prepend(HandleCors::class);
+    })
     ->withExceptions(function (Exceptions $exceptions): void {
 
         $exceptions->render(function (NotFoundHttpException $e, Request $request) {
